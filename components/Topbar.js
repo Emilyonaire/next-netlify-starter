c80ca2374b263cb454caf9e4b50b0583208a7991
Emilyonaire <emily.cowell.2002@gmail.com>
2022-03-21 22:25:47 +0000

    added topbar container css fropm bulma

--- components/Topbar.js
+++ components/Topbar.js
@@ -3,7 +3,7 @@ import styles from './Topbar.module.css'
 export default function Topbar() {
     return(
         <>
-        <Topbar className={styles.footer}>
+        <Topbar className='container'>
             <img src="/pageIcon.svg" alt='Return to Home' className={styles.logo} />
         </Topbar>
         </>

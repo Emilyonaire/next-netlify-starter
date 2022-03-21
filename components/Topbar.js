2e77ee68350a76b1a0f24f418bca33e772c49c24
Emilyonaire <emily.cowell.2002@gmail.com>
2022-03-21 22:31:25 +0000

    g

--- components/Topbar.js
+++ components/Topbar.js
@@ -4,7 +4,8 @@ export default function Topbar() {
     return(
         <>
         <Topbar className='container'>
-            <img src="/pageIcon.svg" alt='Return to Home' className={styles.logo} />
+            {/* <img src="/pageIcon.svg" alt='Return to Home' className={styles.logo} /> */}
+            <img src="/netliheart.svg" alt='Return to Home' className={styles.logo} />
         </Topbar>
         </>
     )

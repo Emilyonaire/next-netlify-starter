1794605fedac32217683518224c41471b8ef8919
Emilyonaire <emily.cowell.2002@gmail.com>
2022-03-21 22:26:17 +0000

    added topbar to index

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import Topbar from '@components/topBar'
 
 export default function Home() {
   return (
     <div className="container">
+      <Topbar />
       <Head>
         <title>Emily Cowell Portfolio!</title>
         <link rel="icon" href="/favicon.ico" />

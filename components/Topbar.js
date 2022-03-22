07a1f782f90efc25ebc3512aca6ed1e138cbd615
Emilyonaire <emily.cowell.2002@gmail.com>
2022-03-22 12:40:04 +0000

    made small change to topbar

--- components/Topbar.js
+++ components/Topbar.js
@@ -5,7 +5,7 @@ export default function Topbar({ title }) {
     return (
         <>
             <img src="/netliheart.svg" alt="Netlify Logo" className={styles.icon} />
-            <h1 className={`${styles.topbar} ${styles.icon}`}>{title}</h1>
+            <h1 className={`${styles.topbar}`}>{title}</h1>
 
         </>
     )

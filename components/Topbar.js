3a8f0034f26c5f3348c9389e627290b1da6bc1f6
Emilyonaire <emily.cowell.2002@gmail.com>
2022-03-22 18:18:06 +0000

    t

--- components/Topbar.js
+++ components/Topbar.js
@@ -4,7 +4,7 @@ import styles from './Topbar.module.css'
 export default function Topbar({ title }) {
     return (
         <>
-            <nav class="navbar" role={"navigation"} aria-label="main navigation">
+            <nav class="navbar" style="background-color: rgb(31,35,36);" role={"navigation"} aria-label="main navigation">
                 <div className='navbar-brand'>
                     <img src="/netliheart.svg" alt="Netlify Logo" className={styles.logo} href="index/#" />
                 </div>

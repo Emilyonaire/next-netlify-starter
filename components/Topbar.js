1506a201180a964abe6af9f4c42e85d24818b63b
Emilyonaire <emily.cowell.2002@gmail.com>
2022-03-23 23:22:22 +0000

    integrated css from past project

--- components/Topbar.js
+++ components/Topbar.js
@@ -5,13 +5,18 @@ export default function Topbar({ title }) {
     return (
         <>
             {/* <nav class="navbar" style="background-color: rgb(31,35,36);" role={"navigation"} aria-label="main navigation"> */}
-            <nav class="navbar" role={"navigation"} aria-label="main navigation">
+            {/* <nav class="navbar" role={"navigation"} aria-label="main navigation">
                 <div className='navbar-brand'>
                     <img src="/netliheart.svg" alt="Netlify Logo" className={styles.icon} href="index/#" />
 
                 </div>
-            </nav>
-
+            </nav> */}
+            <div className={styles.titleBar}>
+                <img src="/netliheart.svg" alt="Netlify Logo" className={styles.icon} href="index/#" />
+                <span className={styles.pageTitle}>
+                    {title}
+                </span>
+            </div>
         </>
     )
 }

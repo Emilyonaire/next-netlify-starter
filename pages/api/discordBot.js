d412ce98d86976b0958654ff03a841e409081971
Emilyonaire <emily.cowell.2002@gmail.com>
2022-05-16 13:10:40 +0100

    habusuh

--- pages/api/discordBot.js
+++ pages/api/discordBot.js
@@ -1,11 +1,26 @@
+var jsonIHave = {
+    "currentMessage": "fetched from site",
+    "currentAuthor": "author fetched from the site",
+    "currentMessageID": "ID fetched from the site"
+}
+
+var stringIHave = "site response to post request"
+
 export default function handler(request, response) {
     if(request.method === 'POST'){
         // PROCESS POST REQUEST, INCOMING DATA
         console.log(request.body);
-        
 
+        jsonIHave = request.body;
+
+        
+        // RESPOND TO THE REQUEST
+        response.statusCode = 200;
     } else if(request.method === 'GET'){
         // PROCESS GET REQUEST, INCOMING DATA
-        response.status(200).json({"message": "Hello World!"});
+        response.status(200).json({
+            "message": "responded via json as was already in json",
+            "test": stringIHave
+        });
     }
 }

97422e4587cd515e6f637e6ac955debc9700f09d
Emilyonaire <emily.cowell.2002@gmail.com>
2022-05-16 18:38:11 +0100

    maybe?

--- pages/api/discordBot.js
+++ pages/api/discordBot.js
@@ -9,17 +9,17 @@ var stringIHave = "site response to post request"
 export default function handler(request, response) {
     if(request.method === 'POST'){
         // PROCESS POST REQUEST, INCOMING DATA
-        // console.log(request.body);
+        console.log(request.body); 
 
-        // stringIHave = request.body;
-        response.status(200).json({
-            "message": "responded via json as was already in json",
-            "test": "response from post request"
-        });
+        stringIHave = request.body;
+        // response.status(200).json({
+        //     "message": "responded via json as was already in json",
+        //     "test": "response from post request"
+        // });
 
         
         // RESPOND TO THE REQUEST
-        // response.status(200).send();
+        response.status(200).send();
     } else if(request.method === 'GET'){
         // PROCESS GET REQUEST, INCOMING DATA
         response.status(200).json({

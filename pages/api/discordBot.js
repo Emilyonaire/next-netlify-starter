b5b4e3f924ca7897844dd4debd7d269785db324c
Emilyonaire <emily.cowell.2002@gmail.com>
2022-05-16 19:47:28 +0100

    POG IT WORKS BIG POG

--- pages/api/discordBot.js
+++ pages/api/discordBot.js
@@ -9,21 +9,31 @@ var stringIHave = "site response to post request"
 export default function handler(request, response) {
     if(request.method === 'POST'){
         // PROCESS POST REQUEST, INCOMING DATA
-        console.log(request.body); 
+        console.log("========received post request========")
+        console.log(request.body);
+        
+        console.log("========about to parse========")
+        jsonIHave = request.body;
+        console.log("========about to print json========")
+        console.log(jsonIHave);
+        console.log(jsonIHave.currentMessage);
+        console.log(jsonIHave.currentAuthor);
+        console.log(jsonIHave.currentMessageID);
+        console.log("========printed json========")
 
-        // stringIHave = request.body;
         
         
         // RESPOND TO THE REQUEST
         response.status(200).json({
             "message": "responded via json as was already in json",
-            "test": "got post request, stringIHave is now: " + request.body
+            // "test": "got post request, stringIHave is now: " + request.body
         });
     } else if(request.method === 'GET'){
         // PROCESS GET REQUEST, INCOMING DATA
+        console.log("received get request")
         response.status(200).json({
             "message": "responded via json as was already in json",
-            "test": stringIHave
+            "test": JSON.stringify(jsonIHave)
         });
     }
 }
